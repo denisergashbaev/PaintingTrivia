61a9bc2433738c21dce0333d63a4d978c49df95b
Denis Ergashbaev <denis.ergashbaev@gmail.com>
2015-12-25 17:55:13 +0100

    implemented endless loop trivia

--- PaintingTrivia.py
+++ PaintingTrivia.py
@@ -1,7 +1,7 @@
 # all the imports
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash
+    abort, render_template, flash
 from contextlib import closing
 from os.path import abspath, dirname
 
@@ -16,8 +16,19 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 
+#http://stackoverflow.com/questions/3300464/how-can-i-get-dict-from-sqlite-query
+#https://docs.python.org/2/library/sqlite3.html#sqlite3.Connection.row_factory
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
 def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
+    con = sqlite3.connect(app.config['DATABASE'])
+    con.row_factory = dict_factory
+    return con
 
 
 def init_db():
@@ -26,9 +37,9 @@ def init_db():
             print "importing schema"
             db.cursor().executescript(f.read())
 
-        #check if there is data in the database. if so don't update
-        cur = db.cursor().execute('SELECT COUNT(id) FROM painting')
-        painting_count = cur.fetchall()[0][0]
+        # check if there is data in the database. if so don't update
+        cur = db.cursor().execute('SELECT COUNT(id) as count FROM painting')
+        painting_count = cur.fetchone()['count']
         print "painting count is %s" % painting_count
         if painting_count == 0:
             with app.open_resource(DATABASE_FOLDER + '/populate.sql', mode='r') as f:
@@ -50,11 +61,35 @@ def teardown_request(exception):
         db.close()
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def show_entries():
-    cur = g.db.execute('SELECT title, file_name FROM painting')
-    entries = [dict(title=row[0], file_name=row[1]) for row in cur.fetchall()]
-    return render_template('show_entries.html', entries=entries)
+    #initialize the points in the user session
+    if 'right_guesses' not in session:
+        session['right_guesses'] = 0
+        session['wrong_guesses'] = 0
+    #if the request is sent from a form
+    if request.method == 'POST':
+        if int(request.form['chosen_painter']) == session['selected_painting']['id']:
+            session['right_guesses'] += 1
+        else:
+            session['wrong_guesses'] += 1
+
+    # fetch 4 random paintings
+    sql = 'SELECT * FROM painter ORDER BY RANDOM() LIMIT 4'
+    painters = g.db.execute(sql).fetchall()
+    #select the first painter (it is random because the records were selected randomly)
+    selected_painter = painters[0]
+    #http://stackoverflow.com/questions/2279706/select-random-row-from-an-sqlite-table
+    sql = 'SELECT * FROM painting WHERE fk_painter_id = ? ORDER BY RANDOM() LIMIT 1'
+    cur = g.db.execute(sql, [selected_painter['id']])
+    selected_painting = cur.fetchone()
+    session['selected_painting'] = selected_painting
+    return render_template('show_entries.html',
+                           painters=painters,
+                           selected_painter=selected_painter,
+                           selected_painting=selected_painting,
+                           right_guesses=session['right_guesses'],
+                           wrong_guesses=session['wrong_guesses'])
 
 if __name__ == '__main__':
     init_db()
