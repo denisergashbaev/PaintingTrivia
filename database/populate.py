9ccf50d431db46e7c54047fd02610bd442fc2881
Jeronics <jeronicarandell@gmail.com>
2015-12-27 15:23:19 +0100

    Add paintings impressionists

--- database/populate.py
+++ database/populate.py
@@ -10,14 +10,24 @@ painters = {
     'painter1': Painter(u'Vincent van Gogh'),
     'painter2': Painter(u'Pierre-Auguste Renoir'),
     'painter3': Painter(u'Édouard Manet'),
-    'painter4': Painter(u'Claude Monet')
+    'painter4': Painter(u'Claude Monet'),
+    'painter5': Painter(u'Alfred Sisley'),
+    'painter6': Painter(u'Camille Pissarro'),
+    'painter7': Painter(u'Mary Cassatt'),
+    'painter8': Painter(u'Berthe Morisot'),
+    'painter9': Painter(u'Armand Guillaumin'),
+    'painter10': Painter(u'Gustave Caillebotte'),
+    'painter11': Painter(u'Georges Seurat'),
+
 }
 
 paintings = [
     Painting(painters['painter1'], u'Starry Night', '1024px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg'),
     Painting(painters['painter2'], u'Bal du moulin de la Galette', 'Bal_moulin_Galette_renoir.jpg'),
+    Painting(painters['painter2'], u'Le déjeuner des canotiers', 'renoir_dejeurner_canotier.jpg'),
     Painting(painters['painter3'], u'Chez le père Lathuille', 'at-father-lathuille.jpg'),
     Painting(painters['painter4'], u'Le Déjeuner sur l’herbe', 'Monet_dejeunersurlherbe.jpg'),
+    Painting(painters['painter4'], u'The Cliffs at Etretat', 'Claude_Monet_The_Cliffs_at_Etretat.jpg'),
 ]
 
 for painter in painters.values():
