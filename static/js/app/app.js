3916e27ccfa9f90745980c238e71d3ba435d9c19
Jeronics <jeronicarandell@gmail.com>
2016-01-20 12:13:01 +0100

    About to work in other branch

--- static/js/app/app.js
+++ static/js/app/app.js
@@ -2,7 +2,6 @@
 //https://toddmotto.com/mastering-the-module-pattern/
 var app = (function () {
     var app = {};
-
     app.submitGuess = function (btn, form, correct_id) {
         var btn = $(btn);
         var form = $(form);
@@ -10,15 +9,19 @@ var app = (function () {
         function s(bc1) {
             btn.css('backgroundColor', bc1);
             setTimeout(function () {
-                //btn.css('backgroundColor', bc2);
-                //btn.html(text);
+                btn.css('backgroundColor', bc1);
+//                btn.html(text);
                 //http://stackoverflow.com/questions/4605671/jquery-submit-doesnt-include-submitted-button
+                form.empty();
                 btn.appendTo(form);
+                console.log(form.context);
+//                console.log(btn.context);
                 form.submit();
-            }, 500);
+//                setTimeout(function (){},10000)
+            }, 1000);
         }
-
         s(correct_id == btn.val() ? "#00ff00" : "#ff0000");
+        return false;
     };
 
     return app;
