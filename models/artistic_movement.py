0c7d3f092c86bcaa5fda7fc917d2d4f662d7e336
Jeronics <jeronicarandell@gmail.com>
2016-01-23 19:27:58 +0100

    Start Adding artistic movement.

---
+++ models/artistic_movement.py
@@ -0,0 +1,12 @@
+from settings import db
+
+
+class ArtisticMovement(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<ArtisticMovement %r>' % self.name
--- models/painter.py
+++ models/painter.py
@@ -1,9 +1,18 @@
 from settings import db
+from sqlalchemy.orm import relationship, backref
+
+
+artistic_movements = db.Table('artistic_movement',
+    db.Column('painter_id', db.Integer, db.ForeignKey('painter.id')),
+    db.Column('artisticmovement_id', db.Integer, db.ForeignKey('artisticmovement.id'))
+)
 
 
 class Painter(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
+    artistic_movements = relationship('ArtisticMovement', secondary=artistic_movements,
+                             backref=db.backref('painters', lazy='dynamic'))
 
     def __init__(self, name):
         self.name = name
