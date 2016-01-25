8fd238cbb528db06f47750fac9a17489eafbc1c6
Jeronics <jeronicarandell@gmail.com>
2016-01-25 10:10:59 +0100

    Fix master branch by solving syntax error and changing the relationship input type.

--- models/painter.py
+++ models/painter.py
@@ -1,17 +1,17 @@
 from settings import db
 from sqlalchemy.orm import relationship, backref
-
+from models.artistic_movement import ArtisticMovement
 
 artistic_movements = db.Table('painters_to_artistic_movements',
     db.Column('painter_id', db.Integer, db.ForeignKey('painter.id')),
     db.Column('artistic_movement_id', db.Integer, db.ForeignKey('artistic_movement.id'))
-
+                              )
 
 
 class Painter(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
-    artistic_movements = relationship('ArtisticMovement', secondary=artistic_movements,
+    artistic_movements = relationship(ArtisticMovement, secondary=artistic_movements,
                              backref=db.backref('painters', lazy='dynamic'))
 
     def __init__(self, name):
